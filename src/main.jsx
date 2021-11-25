12c4a3b87bd85a0ddd14ed3dd7e4b45dd706596d
fudhoil <71709328+fudhoil@users.noreply.github.com>
2021-11-26 04:31:40 +0700

    ok

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,26 @@
 import React, { Component } from "react";
+import Counters from "./components/counters";
 
 class Main extends React.Component {
+  state = {
+    value: 0,
+  };
+
+  handleIncrement = () => {
+    this.setState({ value: this.state.value + 1 });
+  };
+
   render() {
-    return <div>Hello</div>;
+    return (
+      <div>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-primary btn-md"
+        >
+          next
+        </button>
+      </div>
+    );
   }
 }
 
