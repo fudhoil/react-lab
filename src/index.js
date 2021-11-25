751590d7bb83f6beda273363a3127731e372bff2
fudhoil <71709328+fudhoil@users.noreply.github.com>
2021-11-26 04:08:28 +0700

    ok

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
-import Counters from './components/counters';
+// import Counters from './components/counters';
+import Main from './main';
 
-ReactDOM.render(<Counters />, document.getElementById('root'));
+ReactDOM.render(<Main />, document.getElementById('root'));
