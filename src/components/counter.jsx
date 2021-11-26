b0a9d47f60576a12a3436dd833a6cc6db96b12bb
fudhoil <71709328+fudhoil@users.noreply.github.com>
2021-11-26 13:24:26 +0700

    ok

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,11 +1,5 @@
 import React, { Component } from "react";
-
 class Counter extends React.Component {
-  state = {
-    value: this.props.value,
-    // tags: [],
-  };
-
   //   renderTags() {
   //     if (this.state.tags.length === 0) return <p>There are no tags</p>;
 
@@ -23,34 +17,34 @@ class Counter extends React.Component {
   //     this.handleIncreament.bind(this);
   //   }
 
-  handleIncreament = () => {
-    this.setState({ value: this.state.value + 1 });
-  };
-
   render() {
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={this.handleIncreament}
+          onClick={() => this.props.onIncrement(this.props.counter)}
           className="btn btn-secondary btn-sm"
         >
           Increment
         </button>
-        {/* {this.state.tags.length === 0 && "Please create a new tag!"}
-        <ul>{this.renderTags()}</ul> */}
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
+        </button>
       </div>
     );
   }
 
   getBadgeClasses() {
     let classes = "badge m-2 bg-";
-    classes += this.state.value === 0 ? "warning" : "primary";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { value: value } = this.state;
+    const { value: value } = this.props.counter;
     return value === 0 ? "Zero" : value;
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
-// import Counters from './components/counters';
-import Main from './main';
+import Counters from './components/counters';
 
-ReactDOM.render(<Main />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
