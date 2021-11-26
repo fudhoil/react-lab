dd72f908f81c47b3ab098dc7f678e49b981b4c09
fudhoil <71709328+fudhoil@users.noreply.github.com>
2021-11-26 14:58:01 +0700

    ok

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ class App extends React.Component {
 
   handleClick = (i) => {
     this.setState({ clickedText: texts[i] });
+    i++;
   };
 
   render() {
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import '../node_modules/bootstrap/dist/css/bootstrap.css';
-import Counters from './components/counters';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
