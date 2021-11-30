d602f7a32e6bd89ac30bfbb2de954163e0e1571d
fudhoil <71709328+fudhoil@users.noreply.github.com>
2021-12-01 00:38:57 +0700

    card

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 const NavBar = () => {
   return (
     <div className="navbar bg-primary">
-      <span className="badge bg-light text-primary m-2 mt-1 mb-1">Navbar</span>
+      <span className="badge bg-light text-primary m-2 mt-1 mb-1">Usumin</span>
     </div>
   );
 };
