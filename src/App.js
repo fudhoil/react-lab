29608fe8f530547916854cccc9bb655d0e1f0e4f
fudhoil <71709328+fudhoil@users.noreply.github.com>
2021-11-30 23:22:25 +0700

    ok

--- src/App.js
+++ src/App.js
@@ -2,31 +2,11 @@ import { react } from "@babel/types";
 import React, { Component } from "react";
 import NavBar from "./components/navbar";
 
-const texts = ["Text 1", "Text 2", "Text 3", "Text 4"];
-
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      clickedText: "",
-    };
-  }
-
-  handleClick = (i) => {
-    this.setState({ clickedText: texts[i] });
-    i++;
-  };
-
   render() {
-    const { clickedText } = this.state;
     return (
       <div>
-        {texts.map((text, i) => (
-          <button key={i} onClick={() => this.handleClick(i)}>
-            Click me {i + 1}
-          </button>
-        ))}
-        {clickedText && <p>I clicked on button with text: {clickedText}</p>}
+        <NavBar />
       </div>
     );
   }
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,16 +1,7 @@
 import React, { Component } from "react";
 
-const NavBar = ({ totalCounters }) => {
-  return (
-    <nav className="navbar navbar-light bg-light">
-      <a className="navbar-brand" href="#">
-        Navbar{" "}
-        <span className="badge badge-pill badge-secondary bg-secondary">
-          {totalCounters}
-        </span>
-      </a>
-    </nav>
-  );
+const NavBar = () => {
+  return <div className="navbar bg-primary"></div>;
 };
 
 export default NavBar;
